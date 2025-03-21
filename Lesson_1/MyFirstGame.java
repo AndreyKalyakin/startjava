1ca0fb11eaabb566421e430299b217129dcd5344
Andrey_K <kalyakin200593@gmail.com>
2025-03-21 13:27:08 +0300

    Добавлены исправления в MyFirstGame.java

--- Lesson_1/MyFirstGame.java
+++ Lesson_1/MyFirstGame.java
@@ -4,20 +4,19 @@ public class MyFirstGame {
         int playerNumber = 65;
         int rangeStart = 1;
         int rangeEnd = 100;
-        while (true) {
-            if (playerNumber < computerNumber && playerNumber >= rangeStart) {
-                System.out.println(playerNumber + " меньше того, что загадал компьютер");
-                playerNumber++;
-            } else if (playerNumber > computerNumber && playerNumber <= rangeEnd) {
-                System.out.println(playerNumber + " больше того, что загадал компьютер");
-                playerNumber--;
-            } else if (playerNumber < rangeStart || playerNumber > rangeEnd) {
+        while (computerNumber != playerNumber) {
+            if (playerNumber < rangeStart || playerNumber > rangeEnd) {
                 System.out.println("Ошибка: недопустимое число");
                 break;
-            } else {
-                System.out.println("Вы победили! Загаданное число: " + computerNumber);
-                break;
+            }
+            if (playerNumber < computerNumber) {
+                System.out.println(playerNumber + " меньше того, что загадал компьютер");
+                playerNumber++;
+            } else if (playerNumber > computerNumber) {
+                System.out.println(playerNumber + " больше того, что загадал компьютер");
+                playerNumber--;
             }
         }
+        System.out.println("Вы победили! Загаданное число: " + computerNumber);
     }
 }
