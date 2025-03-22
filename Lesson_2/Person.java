710b1547c2d1e299ee2e7cbdf8bf35fefeacc30b
Andrey_K <kalyakin200593@gmail.com>
2025-03-22 18:38:03 +0300

    Переделал Lesson_2 в обычную папку

---
+++ Lesson_2/Person.java
@@ -0,0 +1,19 @@
+public class Person {
+        public static void main(String[] args) {
+        System.out.println("1. ПЕРЕВОД ПСЕВДОКОДА НА ЯЗЫК JAVA");
+        boolean maleGender = true;
+        if (!maleGender) {
+            System.out.println("Пол женский");
+        } else {
+            System.out.println("Пол мужской");
+        }
+
+        i
+            System.out.println("Рост меньше 1.8 метра");
+        } else {
+            System.out.println("Рост больше 1.8 метра");
+        }
+
+        char firstLetterName = "Andrew".charAt(0);
+        if (firstLetterName == 'M') {
+}
