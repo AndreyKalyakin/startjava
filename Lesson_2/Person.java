bf9e1ec7f92d93366f6f31cf4e7749a36083cde0
Andrey_K <kalyakin200593@gmail.com>
2025-03-25 14:19:18 +0300

    Добавлен класс Person

--- Lesson_2/Person.java
+++ Lesson_2/Person.java
@@ -1,3 +1,27 @@
 public class Person {
-    что-то написал
+    String gender = "мужской";
+    String name = "Андрей";
+    int height = 179;
+    float weight = 94.3f;
+    int age = 31;
+
+    void walk() {
+        System.out.println(name + " идёт");
+    }
+
+    void sit() {
+        System.out.println(name + " сидит");
+    }
+
+    void run() {
+        System.out.println(name + " бежит");
+    }
+
+    void talk() {
+        System.out.println(name + " говорит");
+    }
+
+    void learnJava() {
+        System.out.println(name + "учит Java");
+    }
 }
