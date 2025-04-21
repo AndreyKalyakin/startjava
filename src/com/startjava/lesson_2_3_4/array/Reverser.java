ad435a56597a1d67886e5378980b954567354558
Andrey_K <kalyakin200593@gmail.com>
2025-04-21 17:25:19 +0300

    Внесены исправления в 1 задачу по Массивам

--- src/com/startjava/lesson_2_3_4/array/Reverser.java
+++ src/com/startjava/lesson_2_3_4/array/Reverser.java
@@ -2,9 +2,16 @@ package com.startjava.lesson_2_3_4.array;
 
 public class Reverser {
     public static void main(String[] args) {
+        print(new int[]{});
         execution(new int[]{});
+
+        print(null);
         execution(null);
+
+        print(new int[]{6, 8, 9, 1});
         execution(new int[]{6, 8, 9, 1});
+
+        print(new int[]{13, 8, 5, 3, 2, 1, 1});
         execution(new int[]{13, 8, 5, 3, 2, 1, 1});
     }
 
@@ -13,41 +20,42 @@ public class Reverser {
             System.out.println("Ошибка: передан null вместо массива. Ожидался массив с целыми числами.\n");
             return;
         }
-
-        int length = values.length;
-        if (length == 0) {
+        if (values.length == 0) {
             System.out.println("Ошибка: массив нулевой длины. Ожидался непустой массив.\n");
             return;
         }
-
-        System.out.print("   До реверса: [");
-        printNumbers(values);
-        System.out.println("]");
-
         reverse(values);
-
-        System.out.print("После реверса: [");
-        printNumbers(values);
-        System.out.println("]\n");
     }
 
     private static void reverse(int[] values) {
         int length = values.length;
         for (int i = 0; i < length / 2; i++) {
             int temp = values[i];
-            values[i] = values[length - 1 - i];
-            values[length - 1 - i] = temp;
+            values[i] = values[--length];
+            values[length] = temp;
         }
     }
 
-    private static void printNumbers(int[] values) {
-        boolean first = true;
-        for (int number : values) {
-            if (!first) {
-                System.out.print(", ");
-            }
-            System.out.print(number);
-            first = false;
+    public static void print(int[] values) {
+        if (values == null) {
+            System.out.println("Ошибка: передан null вместо массива. Ожидался массив с целыми числами.");
+            return;
         }
+        if (values.length == 0) {
+            System.out.println("Ошибка: массив нулевой длины. Ожидался непустой массив.");
+            return;
+        }
+        System.out.print("До реверса: [");
+        for (int i = 0; i < values.length; i++) {
+            if (i > 0) System.out.print(", ");
+            System.out.print(values[i]);
+        }
+        System.out.println("]");
+        System.out.print("После реверса: [");
+        for (int i = values.length - 1; i >= 0; i--) {
+            if (i < values.length - 1) System.out.print(", ");
+            System.out.print(values[i]);
+        }
+        System.out.println("]\n");
     }
 }
